10401b983f39d6f6b1b79ae64f02b9659d9ef65d
axma1503 <axellemartin15031999@gmail.com>
2020-04-03 19:26:47 +0200

    Création de modifAdmin
    Signed-off-by: axma1503 <axellemartin15031999@gmail.com>

--- views/js/code.js
+++ views/js/code.js
@@ -1,10 +1,15 @@
-function header(){ // mettre en paramétre si l'utilisateur s'est déjà connecté
+function header(){ // mettre en paramétre si l'utilisateur s'est déjà connecté et si on est sur la page accueil (reprendre code accueil.html)
     var html="<div class='menuLogo'><a href='accueil.html'><img src='../images/Infinite_measures.jpg' height='50px'/></a></div>"
         +"<ul class='menuUtilisateur'>"
             +"<li><!--notifications--><img src='../images/iconSansNotification.png' /></li>"
             +"<li><!-- Prénom-->Prénom</li>"
             +"<li><!-- Nom-->Nom</li>"
-            +"<li class='photo'><!-- Photo--><img src='../images/iconProfil.jpg' height='50px'/></li>"
+            +"<li class='photo'><!-- Photo--><button onClick='openMenuDeroulant()'><img src='../images/iconProfil.jpg' height='50px'/></button></li>"
+            +"<ul class='menuDeroulant' style='display: none;'>"
+                +"<li><a href='admin.html'> Profil </a></li>"//mettre le profil de l'utilisateur
+                +"<li><a href='modifAdmin.html'> Modifier profil </a></li>"//mettre le profil de l'utilisateur
+                +"<li> Déconnexion </li>"
+            +"</ul>";
         +"</ul>";
     document.getElementById("navHeader").innerHTML = html;
 }
@@ -23,4 +28,12 @@ function footer(){
             +"<li><a href='https://www.twitter.com/'><img src='../images/twitter.png' height='50px' /></a></li>"
         +"</ul>";
     document.getElementById("navFooter").innerHTML = html;
+}
+
+function openMenuDeroulant(){
+    if (document.getElementsByClassName("menuDeroulant")[0].style.display == "none"){
+        document.getElementsByClassName("menuDeroulant")[0].style.display="block";
+    } else {
+        document.getElementsByClassName("menuDeroulant")[0].style.display="none";
+    }
 }
