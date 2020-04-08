9f226a224c8f0dc27776e4e024fc88f490ce7391
axma1503 <axellemartin15031999@gmail.com>
2020-04-08 15:15:56 +0200

    Khaled: html modif Axelle: css, js,html page principale (probleme résultat)
    Co-authored-by: khaled.gharbi@isep.fr <khaled.gharbi@isep.fr>
    Signed-off-by: axma1503 <axellemartin15031999@gmail.com>

---
+++ views/js/profil.js
@@ -0,0 +1,70 @@
+var color= ['red','yellow','green','blue','purple'];
+
+function dernierTest(){
+    let ctx= document.getElementById('resultatDernierTestGraph').getContext('2d'); 
+    let chart = new Chart(ctx, {
+        type: 'polarArea',
+        data: {
+            labels: ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5'],
+            datasets: [{
+                label:'résultat lors du dernier test',
+                backgroundColor: color,
+                data: [20, 50, 75, 80, 10]
+            }]
+        },
+    });
+}
+
+function resultatTest(select){
+    var select = document.getElementById("choix").value;
+    console.log(select);
+    let ctx= document.getElementById('resultatTestGraph').getContext('2d'); 
+    var color= ['red','yellow','green','blue','purple'];
+    if (select=='0'){
+        let chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [new Date(2020, 12, 04), new Date(2020, 12, 02), new Date(2020, 12, 01), new Date(2020, 12, 04), new Date(2020, 12, 07)],
+                datasets: [{
+                    label: 'Test 1',
+                    data: [20, 50 , 75, 80, 10],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[0],
+                },{
+                    label: 'Test 2',
+                    data: [30, 60, 65, 90, 0],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[1],
+                },{
+                    label: 'Test 3',
+                    data: [30, 60, 65, 90, 0],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[2],
+                },{
+                    label: 'Test 4',
+                    data: [30, 60, 65, 90, 0],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[3],
+                },{
+                    label: 'Test 5',
+                    data: [30, 60, 65, 90, 0],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[4],
+                }],
+            },
+        });
+    }else {
+        let chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [new Date(2020, 12, 03), new Date(2020, 12, 02), new Date(2020, 12, 01), new Date(2020, 12, 04), new Date(2020, 12, 03)],
+                datasets: [{
+                    label:'Test ' + select,
+                    data: [20, 50, 75, 80, 10],
+                    backgroundColor: 'rgba(0, 0, 0, 0)',
+                    borderColor: color[select-1],
+                }]
+            },
+        });
+    }
+}
