26c329f04a089fe8e08b06aa7e737aad1e443be2
axma1503 <axellemartin15031999@gmail.com>
2020-04-08 15:15:56 +0200

    Khaled: html modif Axelle: css, js,html page principale (probleme résultat)
    Co-authored-by: khaled.gharbi@isep.fr <khaled.gharbi@isep.fr>
    Signed-off-by: axma1503 <axellemartin15031999@gmail.com>

--- views/js/profil.js
+++ views/js/profil.js
@@ -1,13 +1,13 @@
-var color= ['red','yellow','green','blue','purple'];
+var color = ['red', 'yellow', 'green', 'blue', 'purple'];
 
-function dernierTest(){
-    let ctx= document.getElementById('resultatDernierTestGraph').getContext('2d'); 
+function dernierTest() {
+    let ctx = document.getElementById('resultatDernierTestGraph').getContext('2d');
     let chart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5'],
             datasets: [{
-                label:'Résultat lors du dernier test',
+                label: 'Résultat lors du dernier test',
                 backgroundColor: color,
                 data: [20, 50, 75, 80, 10]
             }]
@@ -24,37 +24,37 @@ function dernierTest(){
     });
 }
 
-function resultatTest(select){
+function resultatTest(select) {
     var select = document.getElementById("choix").value;
-    let ctx= document.getElementById('resultatTestGraph').getContext('2d'); 
-    var color= ['red','yellow','green','blue','purple'];
-    var dateRealisation= [new Date(2020, 11, 01), new Date(2020, 11, 02), new Date(2020, 11, 03), new Date(2020, 11, 04), new Date(2020, 11, 07)];
-    if (select=='0'){
+    let ctx = document.getElementById('resultatTestGraph').getContext('2d');
+    var color = ['red', 'yellow', 'green', 'blue', 'purple'];
+    var dateRealisation = [new Date(2020, 11, 01), new Date(2020, 11, 02), new Date(2020, 11, 03), new Date(2020, 11, 04), new Date(2020, 11, 07)];
+    if (select == '0') {
         let chart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: dateRealisation,
                 datasets: [{
                     label: 'Test 1',
-                    data: [20, 50 , 75, 80, 10],
+                    data: [20, 50, 75, 80, 10],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     borderColor: color[0],
-                },{
+                }, {
                     label: 'Test 2',
                     data: [30, 60, 65, 90, 0],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     borderColor: color[1],
-                },{
+                }, {
                     label: 'Test 3',
                     data: [30, 60, 65, 90, 0],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     borderColor: color[2],
-                },{
+                }, {
                     label: 'Test 4',
                     data: [30, 60, 65, 90, 0],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
                     borderColor: color[3],
-                },{
+                }, {
                     label: 'Test 5',
                     data: [30, 60, 65, 90, 0],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
@@ -62,16 +62,16 @@ function resultatTest(select){
                 }],
             },
         });
-    }else {
+    } else {
         let chart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: dateRealisation,
                 datasets: [{
-                    label:'Test ' + select,
+                    label: 'Test ' + select,
                     data: [20, 50, 75, 80, 10],
                     backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: color[select-1],
+                    borderColor: color[select - 1],
                 }]
             },
             options: {
