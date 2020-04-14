00706790db7f8d7da017842a39dda68565640971
Dragonink <Dragonink@users.noreply.github.com>
2020-04-14 10:07:57 +0200

    Edited index page

---
+++ views/index.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8"/>
+    <title>Infinite Measures</title>
+
+</head>
+
+<body>
+
+    <?php include "_header.php"; ?>
+
+    <main>
+
+    </main>
+
+    <?php include "_footer.html"; ?>
+
+</body>
+
+</html>
