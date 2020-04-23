69f352477c4b3ec0ad81995bf6ea640dd9ca649f
chiaralcs <57535422+chiaralcs@users.noreply.github.com>
2020-04-23 12:05:12 +0200

    Create forgetmdp.js

---
+++ views/js/forgetmdp.js
@@ -0,0 +1,4 @@
+function selectType() {
+    for (const el of document.getElementsByClassName("connexion")) el.setAttribute("disabled", "");
+    for (const el of document.getElementsByClassName("forget")) el.removeAttribute("disabled");
+}
