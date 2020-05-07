58f33f4cecebfbcbfc67479623d434cfe87a0199
chiaralcs <57535422+chiaralcs@users.noreply.github.com>
2020-05-07 11:33:22 +0200

    ajout lien map

--- views/index.php
+++ views/index.php
@@ -64,7 +64,8 @@ if (isset($_GET['validation']) && $_GET['validation']=='medecin'){
             </form>
             <div>
                 <h2>Nous trouver</h2>
-                <img src="images/map.png" />
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2546.0976138211868!2d2.2799196659319434!3d48.82452595810421!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e670797ea4730d%3A0xe0d3eb2ad501cb27!2sISEP!5e0!3m2!1sfr!2sfr!4v1588842549578!5m2!1sfr!2sfr" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>        
         </section>
     </main>
 
