74a6fd4600554f235d8bb7c2551a4d71ff585c2d
Killian B <benourkillian@gmail.com>
2020-05-21 11:44:13 +0200

    emails et nss vérif

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -20,51 +20,42 @@ function error() {
 switch ($type) {
     case "user":
         if (preg_match("/^[1-2]\d{2}(?:0[1-9]|1[0-2])\d{10}$/", $nss) === 1) {
-            $status = insertUser($db, $nss, $firstname, $lastname, $email, $password, $linked_manager);
-            if ($status) {
-                $_SESSION["user_type"] = "user";
-                $_SESSION["user_id"] = $nss;
-                $_SESSION["user_prenom"] = $firstname;
-                $_SESSION["user_nom"] = $lastname;
-                $_SESSION["user_email"] = $email;
-                $_SESSION["user_medecin"] = $linked_manager;
-                $_SESSION["user_tel"] = NULL;
-                $_SESSION["user_share"] = TRUE;
-                header("Location: utilisateur.php", true, 303);
+            if (preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/", $email) === 1) {
+                $status = insertUser($db, $nss, $firstname, $lastname, $email, $password, $linked_manager);
+                if ($status) {
+                    $_SESSION["user_type"] = "user";
+                    $_SESSION["user_id"] = $nss;
+                    $_SESSION["user_prenom"] = $firstname;
+                    $_SESSION["user_nom"] = $lastname;
+                    $_SESSION["user_email"] = $email;
+                    $_SESSION["user_medecin"] = $linked_manager;
+                    $_SESSION["user_tel"] = NULL;
+                    $_SESSION["user_share"] = TRUE;
+                    header("Location: utilisateur.php", true, 303);
+                } else {
+                    error();
+                }
             } else {
-                error();
+                echo "<script>alert('L\'adresse mail est invalide.');</script>";
+                header('refresh: 0; url = inscription.php');
             }
         } else {
-            header('Refresh:0;url=inscription.php');
             echo "<script>alert('Le numéro de Sécurité Sociale est invalide.');</script>";
-        }
-        if (preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/", $email) === 1) {
-            $status = insertUser($db, $nss, $firstname, $lastname, $email, $password, $linked_manager);
-            if ($status) {
-                $_SESSION["user_type"] = "user";
-                $_SESSION["user_id"] = $nss;
-                $_SESSION["user_prenom"] = $firstname;
-                $_SESSION["user_nom"] = $lastname;
-                $_SESSION["user_email"] = $email;
-                $_SESSION["user_medecin"] = $linked_manager;
-                $_SESSION["user_tel"] = NULL;
-                $_SESSION["user_share"] = TRUE;
-                header("Location: utilisateur.php", true, 303);
-            } else {
-                error();
-            }
-        } else {
-            header('Refresh:0;url=inscription.php');
-            echo "<script>alert('L\'email est invalide.');</script>";
+            header('refresh: 0; url = inscription.php');
         }
         exit;
         break;
     case "manager":
-        $status = insertManager($db, $firstname, $lastname, $email, $password, $address);
-        if ($status) {
-            header("Location: index.php?validation=medecin", true, 303);
-            exit;
-        } else { error();}
+        if (preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/", $email) === 1) {
+            $status = insertManager($db, $firstname, $lastname, $email, $password, $address);
+            if ($status) {
+                header("Location: index.php?validation=medecin", true, 303);
+                exit;
+            } else { error();}
+        } else {
+            header('refresh: 0; url = inscription.php');
+            echo "<script>alert('L\'adresse mail est invalide.');</script>";
+        }
         break;
     case "admin":
         if (nbAdmin($db) == 0) {
