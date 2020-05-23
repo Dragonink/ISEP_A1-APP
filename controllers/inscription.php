64e8b4ffbecf2787a875c3078a1b7a37d6098d9e
Killian B <benourkillian@gmail.com>
2020-05-23 13:22:31 +0200

    Correction erreur alert mot de passe non conforme

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -21,7 +21,7 @@ switch ($type) {
     case "user":
         if (preg_match("/^[1-2]\d{2}(?:0[1-9]|1[0-2])\d{10}$/", $nss) === 1) {
             if (preg_match("/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/", $email) === 1) {
-                if (preg_match("#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$#", $password === 1)) {
+                if (preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).{8,}$/", $password) === 1) {
                     $status = insertUser($db, $nss, $firstname, $lastname, $email, $password, $linked_manager);
                     if ($status) {
                         $_SESSION["user_type"] = "user";
@@ -37,7 +37,7 @@ switch ($type) {
                         error();
                     }
                 } else {
-                    echo "<script>alert('Le mot de passe n'est pas conforme.');</script>";
+                    echo "<script>alert('Le mot de passe n\'est pas conforme.');</script>";
                     header('refresh: 0; url = inscription.php');
                 }
             } else {
