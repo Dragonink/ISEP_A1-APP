79c9e029411ff196cced7a6533041d81ab6c2e50
axma1503 <axellemartin15031999@gmail.com>
2020-05-29 12:13:11 +0200

    else
    Signed-off-by: axma1503 <axellemartin15031999@gmail.com>

--- controllers/connexion.php
+++ controllers/connexion.php
@@ -84,7 +84,8 @@ if (preg_match("/^[1-2]\d{2}(?:0[1-9]|1[0-2])\d{8}$/", $account) === 1) {
             invalid_passwd();
         }
         exit;
+    } else {
+        echo "<script>alert(\"Votre compte n'existe pas.\");</script>";
     }
-    echo "<script>alert(\"Votre compte n'existe pas.\");</script>";
 } else {echo "<script>alert('Identifiant invalide.');</script>";}
 ?>
